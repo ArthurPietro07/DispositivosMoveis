41513d53c93fbffc8afcb11fd404148166008d09
Nicolas de Matos Santana Daitx <nicolas.santana1245@gmail.com>
2026-04-01 10:56:56 -0300

    Aula 7

---
+++ aula7/balada.js
@@ -0,0 +1,11 @@
+let idade = 17;
+
+if(idade >= 18) {
+    console.log("Vou dar um rolê na balada")
+}
+else if(idade >= 16 && idade < 18) {
+    console.log("Não tenho idade para ir na balada, mas posso ir no cinema")
+}
+else {
+    console.log("Vou ficar em casa jogando videogame")
+}
