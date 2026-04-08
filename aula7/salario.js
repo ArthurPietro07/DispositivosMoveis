ada90f1d278af6f872e589151b09dc81bcc55558
arthurpietro07 <arthurpietrogalbano@gmail.com>
2026-04-08 11:03:35 -0300

    Aula 7 - laços

---
+++ aula7/salario.js
@@ -0,0 +1,7 @@
+let salario = 1000;
+
+while (salario < 5000) {
+    console.log(`Meu salário ainda é: R$:${salario}`);
+    salario += 100; 
+}
+console.log(`UFA, até que enfim estou ganhando ${salario} reais!!!`); 
---
+++ aula7/whats.js
@@ -0,0 +1,6 @@
+let esperandoResposta = true;
+while (esperandoResposta) {
+    console.log("Esperando Venicio abrir a porta...");
+    esperandoResposta = Math.random() > 0.1; 
+    }
+console.log("Venicio abriu a porta, não irei mais torrar aqui");
